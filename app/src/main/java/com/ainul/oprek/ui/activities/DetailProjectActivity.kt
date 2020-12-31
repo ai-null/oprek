e387503bc402d2bfbf0a7db62b939aa192d5c26d
ai-null <ainul.y9b@gmail.com>
2020-12-31 12:07:41 +0700

    UPDATE: detail project activity

--- app/src/main/java/com/ainul/oprek/ui/activities/DetailProjectActivity.kt
+++ app/src/main/java/com/ainul/oprek/ui/activities/DetailProjectActivity.kt
@@ -8,5 +8,12 @@ class DetailProjectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.setContentView(R.layout.activity_detail_project)
+
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
     }
 }
