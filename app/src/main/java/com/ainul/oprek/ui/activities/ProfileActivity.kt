3e0dab98758032e87085f86cd23ce763149e46ba
ai-null <ainul.y9b@gmail.com>
2021-02-11 23:03:25 +0700

    ADD: bottom sheet dialog to profile to edit data

--- app/src/main/java/com/ainul/oprek/ui/activities/ProfileActivity.kt
+++ app/src/main/java/com/ainul/oprek/ui/activities/ProfileActivity.kt
@@ -2,6 +2,8 @@ package com.ainul.oprek.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
@@ -10,6 +12,8 @@ import com.ainul.oprek.R
 import com.ainul.oprek.databinding.ActivityProfileBinding
 import com.ainul.oprek.ui.viewmodels.MainViewModel
 import com.ainul.oprek.ui.viewmodels.ProfileViewModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -42,5 +46,16 @@ class ProfileActivity : AppCompatActivity() {
                 finish()
             }
         })
+
+        val bottomSheetBehavior = BottomSheetDialog(this, R.style.AppTheme_BottomSheetDialog)
+        bottomSheetBehavior.setContentView(R.layout.bottom_sheet_edit_data)
+
+        binding.inputUsernameLayout.setEndIconOnClickListener {
+            bottomSheetBehavior.show()
+        }
+
+        binding.inputCompanyLayout.setEndIconOnClickListener {
+            Toast.makeText(this, "heyaa", Toast.LENGTH_SHORT).show()
+        }
     }
 }
