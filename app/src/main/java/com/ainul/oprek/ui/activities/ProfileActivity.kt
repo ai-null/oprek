b34c5e3e7a74f372757b90a45bfca8e51c6cf55e
ai-null <ainul.y9b@gmail.com>
2021-02-11 15:41:42 +0700

    ADD: profile viewmodel and fix inputLayout on register & login fragments

--- app/src/main/java/com/ainul/oprek/ui/activities/ProfileActivity.kt
+++ app/src/main/java/com/ainul/oprek/ui/activities/ProfileActivity.kt
@@ -1,16 +1,46 @@
 package com.ainul.oprek.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.ainul.oprek.R
+import com.ainul.oprek.databinding.ActivityProfileBinding
+import com.ainul.oprek.ui.viewmodels.MainViewModel
+import com.ainul.oprek.ui.viewmodels.ProfileViewModel
 
 class ProfileActivity : AppCompatActivity() {
+
+    private val viewmodel: MainViewModel by lazy {
+        ViewModelProvider(
+            this,
+            MainViewModel.Factory(this.application)
+        ).get(MainViewModel::class.java)
+    }
+    private lateinit var binding: ActivityProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_profile)
 
+        // set custom toolbar
         val toolbar: Toolbar = findViewById(R.id.profile_toolbar)
         setSupportActionBar(toolbar)
+
+        binding.lifecycleOwner = this
+        binding.viewmodel = viewmodel
+    }
+
+    override fun onStart() {
+        super.onStart()
+        viewmodel.logoutState.observe(this, {
+            if (it) {
+                val intent = Intent(this, AuthActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        })
     }
 }
